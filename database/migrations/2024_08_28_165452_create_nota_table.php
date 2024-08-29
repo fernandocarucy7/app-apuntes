1d889c109fa4e7ced62a72c48754b2e8ed93aee9
fernandocarucy7 <fercarucy111@gmail.com>
2024-08-29 08:30:04 -0500

    Subiendo el primer commit

---
+++ database/migrations/2024_08_28_165452_create_nota_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nota', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->foreignId('apunte_id')->constrained('apuntes');
+            $table->foreignId('referencia_id')->constrained('referencias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nota');
+    }
+};

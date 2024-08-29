39527144755e61cd190de7b5534d5141a6c2a4e2
fernandocarucy7 <fercarucy111@gmail.com>
2024-08-29 11:37:58 -0500

    subiendo el segundo commit

---
+++ app/Models/Referencia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Referencia extends Model
+{
+    use HasFactory;
+
+    protected $table = 'referencias';
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/prueba', function () {
+    $blog = new Blog;
+    /*
+    $blog->titulo = 'apunte 2';
+    $blog->apuntes = ['este es el apunte 2.1','este es el apunte 2.2','este es el apunte 2.3'];
+    $blog->referencias = ['esta es la referencia 2.1','eta es la referencia 2.2','eta es la referencia 2.3'];
+    $blog->save();*/
+
+    $blog1 = Blog::where('titulo', 'apunte 1')
+                 ->first();
+    
+    $blog1->titulo = 'laravel 11';
+    $blog1->save();
+
+    return $blog1;
+
 });
